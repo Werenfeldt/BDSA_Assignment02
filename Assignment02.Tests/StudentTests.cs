30174dc004560436ecacd00858ce33a95375c965
Werenfeldt <63417041+Werenfeldt@users.noreply.github.com>
2021-09-21 10:46:01 +0200

    change date

--- Assignment02.Tests/StudentTests.cs
+++ Assignment02.Tests/StudentTests.cs
@@ -43,7 +43,7 @@ namespace StudentClass.Tests
             var teststudent = new Student(1234, "Jens", "Jensen", DateTime.Today.AddDays(-100), DateTime.Today.AddDays(200), DateTime.Today.AddDays(95)); ;
 
             //When
-            var expectedstring = "Student id: 1234, \nStudent name: Jens Jensen. \nStatus: Active \nStart and end date: 06/13/2021 : 04/09/2022. \nGraduation date: 25/12/2021. ".Trim();
+            var expectedstring = "Student id: 1234, \nStudent name: Jens Jensen. \nStatus: Active \nStart and end date: 06/13/2021 : 04/09/2022. \nGraduation date: 12/25/2021. ".Trim();
             var actualstring = teststudent.ToString().Trim();
 
             //Then
